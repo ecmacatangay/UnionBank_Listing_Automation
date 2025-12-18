8346b423c4f4cfe668cc953cd7fd142ffd00dc8a
Erson <43384220+ecmacatangay@users.noreply.github.com>
2025-12-18 16:46:11 +0800

    Added Libraries

--- UB_Listing.py
+++ UB_Listing.py
@@ -1,5 +1,15 @@
 
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import (
+    TimeoutException, StaleElementReferenceException,
+    ElementClickInterceptedException, ElementNotInteractableException, NoSuchElementException
+)
+from urllib.parse import urlparse, parse_qs
+import time, csv
+asdasd
 # Keep Chrome Browser Open
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
