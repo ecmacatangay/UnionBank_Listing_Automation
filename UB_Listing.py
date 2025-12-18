1c8d776590fa49e3319d0c65db9a439a0d5d69f7
Erson <43384220+ecmacatangay@users.noreply.github.com>
2025-12-18 16:30:18 +0800

    Remove Libraries

--- UB_Listing.py
+++ UB_Listing.py
@@ -1,14 +1,4 @@
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import (
-    TimeoutException, StaleElementReferenceException,
-    ElementClickInterceptedException, ElementNotInteractableException, NoSuchElementException
-)
-from urllib.parse import urlparse, parse_qs
-import time, csv
 
 # Keep Chrome Browser Open
 chrome_options = webdriver.ChromeOptions()
